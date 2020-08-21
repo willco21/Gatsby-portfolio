6af8ad855ed3e44e3e5ad1660a5bce01248606d8
hidekauzu masui <zyapanetto@gmail.com>
2020-08-21 12:22:05 +0000

    fix crossOrigin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         short_name: `Cara`,
         description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
         start_url: `/`,
-		crossOrigin: `use-credentials`,
+        crossOrigin: `use-credentials`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
         display: `standalone`,
